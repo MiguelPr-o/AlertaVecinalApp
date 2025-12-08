f9f02f15e8b64447a8621c523239697540d491e7
Miguel <miguelgodinez1289@gmail.com>
2025-12-08 13:37:39 -0600

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    // ELIMINA la línea del secrets plugin de aquí
+}
+
+// AGREGA esto para el Secrets Plugin
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
